5239858fad68b2083f90debb3e73b0f3af3eff13
Edgar Medrano <108080807+EdNyD@users.noreply.github.com>
2026-04-10 10:02:00 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+// sw.js - Desinstalador automático
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(names => Promise.all(names.map(n => caches.delete(n))))
+            .then(() => self.registration.unregister())
+    );
+});
